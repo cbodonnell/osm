9e0a03c8f2c6fc288680e6fcb0b48c2fee6b1eca
Craig O'Donnell <cbodonnell@comcast.net>
2020-04-08 23:09:03 -0400

    nodes to ways

--- osm.py
+++ osm.py
@@ -1,19 +1,26 @@
 import requests
 
 if __name__ == '__main__':
-    bbox = "40.698843, -73.915201,40.701080, -73.912077"
-
+    bbox = "40.698843,-73.915201,40.701080,-73.912077"
     overpass_url = "http://overpass-api.de/api/interpreter"
     overpass_query = """
-    [out:json][timeout:25];
-    (
-      node["highway"]({0});
-      way["highway"]({0});
-      relation["highway"]({0});
-    );
+    [out:json][timeout:25][bbox:{0}];
+    way[highway]->.w1;
+    way[highway]->.w2;
+    node(w.w1)(w.w2)->.nodes;
+    way[highway]->.ways;
+    (.nodes; .ways;);
     out body;
     """.format(bbox)
-
     response = requests.get(overpass_url, params={'data': overpass_query})
-    data = response.json()
-    print(data)
+    if response.status_code == 200:
+        data = response.json()
+        elements = data['elements']
+        ways = [e for e in elements if e['type'] == 'way']
+        for way in ways:
+            nodes = way['nodes']
+            way['nodes'] = [e for e in elements if (e['type'] == 'node' and e['id'] in nodes)]
+        print(ways)
+    else:
+        print(response.reason)
+        print(response.text)
